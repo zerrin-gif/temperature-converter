79a06c076a47d35a23fec1cb0f2a66c290d48077
Zerrin Ekici <zerkam06@gmail.com>
2020-10-04 17:26:11 -0500

    round kelvin value to nearest number with two decimal points

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 console.log('it is working');
 let celsius = prompt('Enter the Celsius value') * 1;
-let kelvin = celsius + 273.15
-console.log(`${celsius} Celsius degree equals ${kelvin} Kelvin`);
+let kelvin = celsius + 273.15;
+kelvin = kelvin.toFixed(2);
+alert(`${celsius} Celsius degree equals ${kelvin} Kelvin`);
