7c4cf995ea225bde238993363e07e6d310c149e6
Zerrin Ekici <zerkam06@gmail.com>
2020-10-05 15:22:39 -0500

    add calculations celsius to kelvin and fahrenheit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
-console.log('it is working');
+
 let celsius = prompt('Enter the Celsius value') * 1;
 let kelvin = celsius + 273.15;
+let fahrenheit = (celsius * 9 / 5 ) + 32 
+
+ 
 kelvin = kelvin.toFixed(2);
-alert(`${celsius} Celsius degree equals ${kelvin} Kelvin`);
+alert(`${celsius} Celsius degree equals ${kelvin} Kelvin, and ${fahrenheit} Fahrenheit`);
